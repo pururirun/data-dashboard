897367aee0065adeea91a2a4bb3817ca9ddf2811
pururirun <j.mardonescava@gmail.com>
2017-11-17 14:21:07 -0300

    Data V.2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 /*
  * Funcionalidad de tu producto
  */
+/*Menú despegable*/
+
+var boton = document.getElementById("btn");
+
+function showMenu() {
+  var menu = document.getElementById("opcsMenu");
+
+  if (menu.classList.contains("disabledMenu")){
+    menu.classList.remove("disabledMenu");
+    menu.classList.add("enabledMenu");
+  }
+  else {
+    menu.classList.remove("enabledMenu");
+    menu.classList.add("disabledMenu");
+  }
+}
+
+boton.addEventListener("click", showMenu);
+
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
